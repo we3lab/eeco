8832465bb83c321759ca68f6b16bbbd3206d907a
Fletcher Chapin <fletchapin@gmail.com>
2025-05-12 15:22:19 -0400

    Reverting to Gurobi for nonlinear programs

--- electric_emission_cost/tests/test_utils.py
+++ electric_emission_cost/tests/test_utils.py
@@ -43,7 +43,7 @@ def test_sum_pyo(consumption_data, varstr, expected):
     var = getattr(model, varstr)
     result, model = ut.sum(var, model=model, varstr="test")
     model.obj = pyo.Objective(expr=0)
-    solver = pyo.SolverFactory("gurobi")
+    solver = pyo.SolverFactory("glpk")
     solver.solve(model)
     assert pyo.value(result) == expected
     assert model is not None
@@ -83,7 +83,7 @@ def test_multiply_pyo(consumption_data, varstr1, varstr2, expected):
     var2 = getattr(model, varstr2)
     result, model = ut.multiply(var1, var2, model=model, varstr="test")
     model.obj = pyo.Objective(expr=0)
-    solver = pyo.SolverFactory("glpk")
+    solver = pyo.SolverFactory("gurobi")
     solver.solve(model)
     assert np.allclose([pyo.value(result[i]) for i in range(len(result))], expected)
     assert model is not None
