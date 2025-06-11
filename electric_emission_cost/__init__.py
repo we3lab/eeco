48d6715d7e59bb5943172bb61304011d81aceb2c
Fletcher Chapin <fletchapin@gmail.com>
2025-06-11 13:46:52 -0700

    Bump version: 0.0.2 → 0.0.3

--- electric_emission_cost/__init__.py
+++ electric_emission_cost/__init__.py
@@ -5,7 +5,7 @@ __author__ = "WE3 Lab"
 __email__ = "fchapin@stanford.edu"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 
 def get_module_version():
