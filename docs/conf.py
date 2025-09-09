ba54988e2da701a2043daf9019754231c8f19e12
Fletcher Chapin <fletchapin@gmail.com>
2025-09-09 12:06:12 -0700

    Bump version: 0.0.4 → 0.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = "WE3Lab"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "0.0.4"
+release = "0.0.5"
 
 
 # -- General configuration ---------------------------------------------------
--- electric_emission_cost/__init__.py
+++ electric_emission_cost/__init__.py
@@ -5,7 +5,7 @@ __author__ = "WE3Lab"
 __email__ = "fchapin@stanford.edu"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 
 
 def get_module_version():
