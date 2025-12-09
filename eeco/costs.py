d86ca9c2acae70db950e311db739ac8a6ad9bfe6
Adhithyan Sakthivelu <admkr.2010@gmail.com>
2025-12-09 11:58:44 -0600

    charge_array_fix (#47)
    * charge_array_fix
    
    * Delete Pipfile
    
    * Delete Pipfile.lock
    
    * pytest fix
    
    * Added print messages for debugging
    
    * Fixing off by 1 error with tiered charges
    
    ---------
    
    Co-authored-by: Fletcher Chapin <fletchapin@gmail.com>

--- eeco/costs.py
+++ eeco/costs.py
@@ -808,17 +808,17 @@ def calculate_energy_cost(
         cumulative_consumption = np.cumsum(consumption_estimate) + prev_consumption
         total_consumption = cumulative_consumption[-1]
 
-        start_idx = np.argmax(cumulative_consumption >= float(limit))
-        # if not found argmax returns 0
-        if (start_idx == 0) and (total_consumption < float(limit)):
-            start_idx = -1
-        if np.isinf(next_limit) or (total_consumption < float(next_limit)):
-            end_idx = -1
+        start_idx = np.argmax(cumulative_consumption > float(limit))
+        # if not found argmax returns 0, but whole charge array should be zeroed
+        if (start_idx == 0) and (total_consumption <= float(limit)):
+            charge_array[:] = 0
         else:
-            end_idx = np.argmax(cumulative_consumption > float(next_limit))
-
-        charge_array[:start_idx] = 0  # 0 for charge array before the start index
-        charge_array[end_idx:] = 0  # 0 for charge array after the end index
+            charge_array[:start_idx] = 0  # 0 for charge array before the start index
+        end_idx = np.argmax(cumulative_consumption > float(next_limit))
+        # before applying end_idx ensure (1) that next limit exists
+        # and (2) that it is higher than total consumption
+        if not (np.isinf(next_limit) or (total_consumption <= float(next_limit))):
+            charge_array[end_idx:] = 0  # 0 for charge array after the end index
 
         charge_expr, model = ut.multiply(
             consumption_data, charge_array, model=model, varstr=varstr + "_multiply"
