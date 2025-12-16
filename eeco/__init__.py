63ef64634fa1811b13e4b9c770eed781601c68ab
Fletcher Chapin <fletchapin@gmail.com>
2025-12-16 15:05:50 -0800

    Bump version: 0.1.0 → 0.2.0

--- eeco/__init__.py
+++ eeco/__init__.py
@@ -5,7 +5,7 @@ __author__ = "WE3Lab"
 __email__ = "fchapin@stanford.edu"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.rst
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 def get_module_version():
--- setup.py
+++ setup.py
@@ -73,6 +73,6 @@ setup(
     extras_require=extra_requirements,
     test_suite="tests",
     url="https://github.com/we3lab/eeco",
-    version="0.1.0",
+    version="0.2.0",
     zip_safe=False,
 )
