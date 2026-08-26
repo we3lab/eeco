3db17a3589bd4c62bce558e4e9f58eef6e8c7293
Fletcher Chapin <fletchapin@gmail.com>
2026-08-26 15:50:04 -0700

    Removing todo message and creating GH issue instead

--- eeco/tests/test_utils.py
+++ eeco/tests/test_utils.py
@@ -471,6 +471,3 @@ def test_python_types():
     assert ut.check_nonindexed_pyomo_type(y) is False
     assert ut.check_nonindexed_pyomo_type(z) is False
     assert ut.check_nonindexed_pyomo_type(z[0]) is False
-
-
-# TODO: add test_get_freq_binsize_minutes
